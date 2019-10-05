6268a881b8a7df26bc0f49fc712094e43c0eb877
AlSolor <alansolora@gmail.com>
2019-10-05 14:01:21 -0500

    Segunda sincronizacion. Actividad 3.1. Parte 2

---
+++ RelacionJerarquica3_1.java
@@ -0,0 +1,11 @@
+public class RelacionJerarquica3_1{
+	public static void main(String[] args) {
+		Punto punto=new Punto(30,50);
+		Circulo circulo = new Circulo (120,89,2.7);
+		System.out.println("ACTIVIDAD 3.1");
+		System.out.println("Lllamado a toString de Punto con referencia "+ "a la superclase apuntando al objeto de la superclase: \n"+ punto.toString());
+		System.out.println("Llamado a toString de Circulo con referencia" + " a la subclase apuntando al objeto de la subclase: \n" + circulo.toString());
+		circulo= (Circulo)punto;
+		System.out.println("Lllamdo a toString de Circulo con referencia"+" a la superclase apuntando al objeto e la subclase: \n"+ punto.toString());
+	}
+}
